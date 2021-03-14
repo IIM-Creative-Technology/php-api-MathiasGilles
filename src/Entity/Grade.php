d5f00fc5e892a2c78a233810a088b148646c9e5b
Mathias <mathias.gilles136@gmail.com>
2021-03-14 15:39:40 +0100

    [add] Api Plateform

--- src/Entity/Grade.php
+++ src/Entity/Grade.php
@@ -2,10 +2,12 @@
 
 namespace App\Entity;
 
+use ApiPlatform\Core\Annotation\ApiResource;
 use App\Repository\GradeRepository;
 use Doctrine\ORM\Mapping as ORM;
 
 /**
+ * @ApiResource()
  * @ORM\Entity(repositoryClass=GradeRepository::class)
  */
 class Grade
